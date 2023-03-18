d6ca04bfbb8ddb7b12fd0426c9f62e5791d6846c
kimura <tansen87@qq.com>
2023-03-18 21:49:43 +0800

    update log.py

--- utils/log.py
+++ utils/log.py
@@ -1,7 +1,7 @@
 '''
 Author: tansen
 Date: 2023-03-18 17:41:48
-LastEditors: Please set LastEditors
+LastEditors: tansen
 LastEditTime: 2023-03-18 18:09:23
 '''
 import logging
@@ -24,11 +24,3 @@ class Log:
     @staticmethod
     def error(message: str):
         logging.error(f"\033[0;31m{message}\033[0m")
-
-
-if __name__ == '__main__':
-    # 测试代码
-    Log.info("info日志")
-    Log.warning("warning日志")
-    Log.error("error日志")
-    Log.debug("debug日志")
